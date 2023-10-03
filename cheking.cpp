3b047b703dfd0d37f18d102dd6cfaa0a5ef60325
Lutfor Rahman Hera <87908048+swehera@users.noreply.github.com>
2023-10-03 11:57:00 +0600

    Create cheking.cpp
    it's a c++ function for cheking info

---
+++ cheking.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  string name;
+  int roll;
+
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Enter your roll: ";
+  cin >> roll;
+
+  if(name == "hira" && roll==141168) {
+    cout << endl;
+    cout << endl;
+    cout << "Welcome to network";
+  }else {
+    cout << endl;
+    cout << endl;
+    cout << "Who are you?";
+  }
+
+  
+  return 0;
+}
